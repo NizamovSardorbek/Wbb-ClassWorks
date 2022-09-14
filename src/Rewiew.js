612bb85b3b245da3ffe54986c69733c83df01b49
SardorbekDevelop <sardornizamov717@gmail.com>
2022-09-14 15:54:41 +0500

    rewiews

---
+++ src/Rewiew.js
@@ -0,0 +1,86 @@
+// Git hub bu online store yani hamma koraoladigan va foydalanaoladigan online store
+
+// Branch bu katta store yani hamma kurish imkoniyatga ega store unda hamma yuklab oladi 
+// Branch bu filialga oxshagan  narsa branchlarda 
+// branchdan branchga otish uchun git checkout master 
+
+
+// 1 git add . bu osha papkani qolga olish 
+// 2 git commit -m 'darslik'
+// git push origin master yani branchni nomi yoziladi
+
+
+// Pull  
+// 1- git pul origin branchname // Pull  
+
+// Repository 
+// 1 git init 
+// 2 git remote add origin https://github.com/SardorbekDevelop/Workin.git
+// 3 git remote -v
+// 4 git add .
+// 5 git commit -m 'comment'
+
+// Repository 
+// 1 git init 
+// 2 git remote add origin https://github.com/SardorbekDevelop/Workin.git
+// 3 git remote -v
+// 4 git add .
+// 5 git commit -m 'comment'
+
+
+// gut pull origin (navbar) yani branchdan branchga malumotni olish uchun shuni yozsak bishqa branchdagi maluotqmzni hammasi    masterga otadi 
+
+
+
+
+////////////    R   E   A   C   T        ///////////////
+////////////    R   E   A   C   T        ///////////////
+////////////    R   E   A   C   T        ///////////////
+////////////    R   E   A   C   T        ///////////////
+
+// react bu hamma foydalanishi mumkin bolgan kutubxoba  
+// nmaga react tez ishlaydi ? - single page application
+// reactda tortta asosiy hususiyat bor 1-vertual dom borligi  2-unidirectional data flow 3-components 3server Side rendering
+
+
+// 1 Virtual dom - rerender bolishini oladi yani vertual dom ishlatmasak biz ozgartradgn qismi ozgarmasda hammasini boshidan chizib kegn osha joyi ozgaradi
+// real dom bn vertualdomni farqi real domda child ozgarsa parentga tasiq qiladi verttualda esa tasir qilmaydi 
+// 2 component qism qismga bolib ishlashga aytiladi 
+// 3 unidirectional data flow parentda childga malumot keladi lekin childddan parentga malumot bormedi 
+// 4 server Side Renderending bu beceknd tomondan manikulyatsiya qilib ekranga chiqarishga aytiladi buning afzalliklari data tez yuklanb becekndga borib kelib tez ekranga chiqadi biz bajarga amallarni sekin birinma ketn qilib utrmedi 
+
+// babel react  bu yangi avlod yani brauzer eskitta bor  biz yozgan kodni javascript kodiga aylantrish yani javascriptga uzini tiliga otkazb buyruq beradi  
+
+
+
+
+
+///////////  K I R I SH ///////////////
+
+// npx create-react-app (papka nomin) yani papkani ochish create qilish
+// git ignore  file  buni icida biz githubga push bolmaydiganlar turadi yani ignore 
+// readme.md esa qollanma yani reactda qandey foydalnishmz haqida qisqacha qollanma 
+// npm run start yani proektni yurgizb beradi va avtamatik tarzda yangilab turadi huddi htmlda golivega oxshab
+
+
+// package.json tasavvur qileli proekt mashina moshinani texpassporti boladi rangi qanaqa matori qande va hokazo
+// dependencies  reactda qandey kutubxonalar ishlatganlarmz turadi
+// scripts aynan qaysi buyruqni beryotkanmzni aniqlab berib turadi
+
+// package-lock.json biz kutiubxonalar ishkatganmz bu fileda esa bizga internetdagi manzilin olib kelib beradi 
+
+// public bunda root bor va react single page application deyilishini sababi ham shu root degan html papkaga hamma malumotlar kelib joylashadi
+
+
+//src bunda npm start degan vaqtmzda buni icidagi index js filemz yuradi 
+// index js da create root qilib publicdagi root degan html fileni yasab olyaopti
+// render bu ekranga chiqarish yani renderni icida boladi ekranga chiqadigan malumotlarmz 
+// root uziga bitta parametr  oladi  yani hamma narsamz renderda iciga bir divga oralga bolishi kerak yoki bitta html kodi yozishmz kerak 
+// reactstricmode bu yani ogohlantrb turadi eski malumot ishlatyapsn seni proektninga tasiri bor deb yozsak ham boladi strict modeni yozmasak ham
+
+//  export agar bir xil nomlar kelib qolsa biz Card as Cards deb nomini uzimz xoxlagandek bervolsak boladi 
+
+// 
+
+
+
