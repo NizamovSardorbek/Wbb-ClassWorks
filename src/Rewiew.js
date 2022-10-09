2f5436187e718af7d7a50b435be6419fc4bdc0c2
SardorbekDevelop <sardornizamov717@gmail.com>
2022-10-09 09:15:14 +0500

    usestate rewiew

--- src/Rewiew.js
+++ src/Rewiew.js
@@ -97,3 +97,41 @@
 // setState asinxron funksya emas callvacbka oxshab kyuga otadi 
 
 // CRUD
+
+ //////////                U  S  E      S  T  A  T  E             //////////
+ //////////                U  S  E      S  T  A  T  E             //////////
+
+ 
+// 1  Usestate birinci elemnti arrey ikkinchi funksya boladi
+// 2  use stateda bergan qiymatmz birinchi paramaterga teng yani arrayga
+// 3 ikkinchi elementi this.setState bn bir xil vazifani bajaradi yani qiymatni ozgartiradi
+// 4 malumotlarmzni chiqarmoqchi bolsak {} qavsda yozamz this ishlatilmedi arrowda
+// 5 yani javascriptda organganmzde arrow funktion da this ishlatilmedi
+// 6 useState bitta clean code yani qoida emas lekn hamma ucuu  odat va tushnarli bulib qolgan usestate ikkinchi paramterga
+// birinchi paramaterga mos kelishi ucun set qoshib yozsak szni codiz tushinarli boladi
+// 7 endi countni ozgartrmoqchimz setcount deymzda count + 1 deb funksya yozamz
+// 8 va yana bir qoida Hooksda biz componentni nomini kicik harfda yozaolmemz
+// 9 inputni icidagi qiymatni olish ucun ham classdagindek setTitlega beramz qiymatni va inputdagini chiqarsh ucun titleni pasdan beramz
+// 10 props bu yerda tepada ocgan compinentmzda () oddiy qavsini icida propsni beramz va qayerda props kerak bolsa props. nomini yozamz boldi
+
+
+
+ //////////                U  S  E      E  F  F  E  C   T             //////////
+ //////////                U  S  E      E  F  F  E  C   T             //////////
+
+
+ // 1 UseEffect componentni icidagi birorta state ozgarganida yoki prop kelsa ana shu use Effectni uzi ishledi
+// 2 UseEffectni bir tort hil caseda ishlataolamz
+// 3 UseEffect doim callback funksya sifatida ishlatiladi
+// 4 Case 1 biz stateni ozgartamzmi inputga malumot yozamzmi bizda case 1 ishledi
+// 5 Case 2 biz beckenddan malumot keladi yani biz case ikkinchini ishlatshmz ucun
+// ikkinchi paramater qoyishmz kerak shunda case ikki boladi
+// yani birinchi paramater callback funksya ikkinchisi ess arrey arrey qoymasak
+// yani ikkinchi paramter bolmasa bu birinchi casee bolib qoladi
+// 6 case ikki component yurganda boshida bir marta yuradi boldi
+// 7 case bu dependensis yani bogliqlik yani biz arreyni iciga bir state 
+// yoki ozgartradigan narsamzni yozamz va osha ozgarsa case 3 ishledi yana
+// ham tushinarli aytadgan bolsa case uc qaysi qiymatni berb qoygan bolsa osha pzgarganda
+// ishledi boshqa bir state yoki boshqa narsa ozgarganda ishleamde
+// 8 casee 4  case 3ni multiple xolati yani arreyga bir 
+// qanca stateni biriktrb qoyamz va yozgnlarmzda case 4 ishledi
