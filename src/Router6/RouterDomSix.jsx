3ec508c92b958733653a3c07ef5404c19adf6e4c
SardorbekDevelop <sardornizamov717@gmail.com>
2022-11-08 10:29:23 +0500

    router olti

---
+++ src/Router6/RouterDomSix.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const RouterDomSix = () => {
+  return (
+    <div>RouterDomSix</div>
+  )
+}
+
+export default RouterDomSix
+
+
+// 1 npm i react-router-dom bu routerni oltinchi versiyasini ornatadi
--- src/index.js
+++ src/index.js
@@ -2,15 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
+import RouterDomSix from './Router6/RouterDomSix';
 import RootRouter from './RouterDom/RootRouter';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-           <RootRouter/>
-    </BrowserRouter>
+<RouterDomSix/>
   </React.StrictMode>
 );
 
